3d917d8cbab4a628b986c4f4110a51472203e233
Ryan Sutcliffe <ryan.sutcliffe@flutterukii.com>
2024-05-14 13:29:18 +0100

    small changes made following call with supervisor

--- src/components/Pages/ReferralHomePage.jsx
+++ src/components/Pages/ReferralHomePage.jsx
@@ -10,16 +10,17 @@ function ReferralHomePage() {
       <div className='ImageContainer'>
       <img src="referafriend.png" alt="Refer A Friend"/> 
       </div>
-      <div className='TextContainer'>
-          <h2>How it Works</h2>
-          <p>1. Enter the email address of the friend you would like the refer</p>
-          <p>2. They will recieve an email with instructions on how to register with SkyBet</p>
-          <p>3. During the registration they will be able to enter your email address</p>
-          <p>4. Once verified you and your friend will recieve a email to confirm</p>
-          <p>5. Bonus is recieved</p>
+        <div className='TextContainer'>
+            <h2>How it Works</h2>
+            <p>1. Enter the email address of the friend you would like to refer.</p>
+            <p>2. They will receive an email with instructions on how to register with SkyBet.</p>
+            <p>3. During the registration, they will be able to enter your email address.</p>
+            <p>4. Once verified, you and your friend will receive an email to confirm the referral.</p>
+            <h3>5. A £10 bonus is received by you.</h3>
+            <h3>6. A £10 bonus is received by your friend.</h3>
 
-      </div>
-      <div className='ButtonContainerHomepage'>
+        </div>
+        <div className='ButtonContainerHomepage'>
           <div className='ReferNewCustomerButton'>
             <Link to="/refernewcustomer">
               <Button variant="contained" color="success">
