33effb6aaf2ac3e41d5378876dc907ec03a5a5f6
Ryan Sutcliffe <ryan.sutcliffe@flutterukii.com>
2024-05-15 14:05:07 +0100

    [CRAF-9] - Made some changes to the email but nothing of note

---
+++ src/cypress/integration/layout.spec.js
@@ -0,0 +1,11 @@
+context('Header', () => {
+    it('should show a header component with title', () => {
+        cy.visit('http://localhost:3000')
+    })
+})
+
+context('Footer', () => {
+    it('should show a footer component', () => {
+        cy.visit('http://localhost:3000')
+    })
+})
